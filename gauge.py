6328dd610008b5b8bd0b92923abb864591301ddb
miccls <martincsvardsjo@gmail.com>
2023-06-09 10:50:43 +0200

    Added some fkn narwhaal shit

---
+++ gauge.py
@@ -0,0 +1,7 @@
+
+
+class Gauge:
+
+    def __init__(self, master, label, write_com, read_com):
+        '''Initializes a gauge to be placed in the gui.
+        '''
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 ''' Here some code will be written,
 NAWRAHL CABON BRAINWOW
 '''
+import tkinter as tk
+
+
+class GUI:
+    '''
+    Our main class
+    '''
+    def __init__(self):
+        '''Initializes variables'''
+
+        pass 
+
+    def mainloop(self)
+
+
+if '__main__' == __name__:
+    gui = GUI()
+    gui.mainloop()
