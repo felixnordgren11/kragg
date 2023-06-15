8dba570835bf8c4ce80b1eb5ffb70d6a2e8f1e14
felixnordgren11 <felix.edu.nordgren@gmail.com>
2023-06-15 15:04:54 +0200

    1

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class GUI:
     Our main class
     '''
     
+    
+    
     def __init__(self):
         '''Initializes variables'''
 
@@ -26,7 +28,7 @@ class GUI:
         self.root.geometry(self.settings.geometry)
         
 
-        # Bind click event
+        # Bind click events
 
         self.root.bind_all("<Key>", self.key)
         self.root.bind_all("<Button-1>", self.callback)
@@ -34,22 +36,32 @@ class GUI:
         self.canvas.focus_set() 
         self.draw_border()
         
-        #Enable buttonss
+        #Enable buttons
+        
         self.btns = {}
         for label, btn in self.settings.buttonsettings.items():
             self.btns[label] = Button(self.canvas, label, **btn)
             self.btns[label].draw()
+            
+        # Loop through the gauge settings dictionary we created in the settings.py file.
+        # For each gauge, we create a Gauge object and store it in the gges dictionary.
+        # Call the draw() method to draw the gauge.
         
         self.gges = {}
         for label, gge in self.settings.gaugesettings.items():
             self.gges[label] = Gauge(self.canvas, label, **gge)
             self.gges[label].draw()
             
+
+        #Mock output values    
         self.gges['v_out'].set_gauge(5.00)
         self.gges['i_out'].set_gauge(3.00)
 
 
-
+    #Function that draws the border of the screen.
+    #Rounded rectangle for the border. 
+    #Create rectangle for the title box. 
+    #Create text for the title
     def draw_border(self):
 
         self.round_rectangle(self.canvas, self.settings.width*0.05, self.settings.height*0.05, 
@@ -62,7 +74,9 @@ class GUI:
         self.canvas.create_text(self.settings.width*0.5, self.settings.height*0.1, text = 'Neanderball', font = ('Small Fonts', 20), fill = 'black')  
     
 
-
+    
+    #Define the round rectangle function
+    
     def round_rectangle(self, master, x1, y1, x2, y2, r=25, **kwargs):    
         points = (x1+r, y1, x1+r, y1, x2-r, y1, x2-r, y1, x2, y1, x2, y1+r,
                 x2, y1+r, x2, y2-r, x2, y2-r, x2, y2, x2-r, y2, x2-r, y2,
@@ -71,28 +85,37 @@ class GUI:
         return master.create_polygon(points, **kwargs, smooth=True)
     
     
+    
+    
     #Activate the gauge when pressing a button:
     
     def power_value(self):
             v = self.gges['v_out'].get_value()
             i = self.gges['i_out'].get_value()
             self.gges['p'].set_gauge(i*v)
+            
+    
 
     
-    def key(self, event):
-        print(event.keysym)
+    def key(self, event:tk.Event):
+        #When either v or i is pressed, switch the active gauge to that and deactivate the other one
         if event.char in ['v','i']:
             sel,notsel = ('v_set','i_set') if event.char == 'v' else ('i_set','v_set')
+            if not isinstance(self.gges[notsel], Gauge):
+                raise TypeError("Expected type Gauge for %s, but got %s instead."%(notsel,type(self.gges[notsel])))
             if self.gges[notsel].get_active():
                 self.gges[notsel].set_active(Lie)
             are_active = self.gges[sel].get_active()
             self.gges[sel].set_active(not are_active)
-
-            
+            return
+        
+        
+        #Bind left and right key buttons to move the active digit in the active gauge
         if event.keysym in ['Left','Right']:
             self.gges['v_set'].move_select(self.settings.moves[event.keysym])
             self.gges['i_set'].move_select(self.settings.moves[event.keysym])
 
+        #Bind up and down to increase/decrease the active digit in the active gauge.
         if event.keysym in ['Up','Down'] and (self.gges['v_set'].get_active() or self.gges['i_set'].get_active()):
             value = 1 if event.keysym == 'Up' else -1
             if self.gges['v_set'].get_active():
@@ -103,7 +126,6 @@ class GUI:
     
     def callback(self, event):
             x, y = event.x, event.y
-            print(x,y)
             # Helper function
             inside_btn = lambda x, y, btn: (x > btn.kwargs['x1'] and x < btn.kwargs['x2']) and (y > btn.kwargs['y1'] and y < btn.kwargs['y2']) 
             # Check if one of boxes are clicked.
