a90fc44ec150325932108c31f62bfef1d1785c1f
miccls <martincsvardsjo@gmail.com>
2023-06-28 11:23:26 +0200

    changed screen size

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings:
 
     def __init__(self):
         self.title = 'GUI'
-        self.geometry = "480x800"
+        self.geometry = "320x480"
         self.width, self.height = map(int, self.geometry.split('x'))
         self.bg = 'black'
         self.textcolor = 'white'
