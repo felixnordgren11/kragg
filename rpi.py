09431cc85c08236ca7d8d0f2a6ea666a75240778
miccls <martincsvardsjo@gmail.com>
2023-07-05 16:09:02 +0200

    MED NOCCO

--- rpi.py
+++ rpi.py
@@ -100,7 +100,7 @@ class RPI:
         if tpe == WRITE:
             if (msg_data[-1] >> 4) != 0xF:
                 # +1 is to compensate for 485 setting 
-                # out max when sent 0.
+                # out max when sent 0. Nocco
                 value = int(100*value) + 1
                 # Data is divided into bytes.
                 MSB = value >> 8
