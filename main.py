af36c0cf559f698f3e13b2b4e705a0c56ee85485
miccls <martincsvardsjo@gmail.com>
2023-07-25 16:35:01 +0200

    zheish

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ class GUI:
             READ, self.settings.command_lib['v_read']), self.rpi.send_msg(READ, self.settings.command_lib['i_read'])
         # Adjust measurement
         #######################
-        v_value = v_value - (27 + 4*(i_value/100) - 1.14*(v_value/100))
+        v_value = v_value - (27 + .4*(i_value/100) - 1.14*(v_value/100))
         #######################
         self.gges['v_out'].set_gauge(v_value/100)
         self.gges['i_out'].set_gauge(i_value/100)
