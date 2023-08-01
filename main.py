915d7b7365324b88c3ba63f30f568ffa0e60c65a
miccls <martincsvardsjo@gmail.com>
2023-08-01 14:37:36 +0200

    cal procedure updated.

--- main.py
+++ main.py
@@ -252,9 +252,10 @@ class GUI:
         self.rpi.send_msg(WRITE, self.settings.command_lib['i_set'], value = current + 1)
         # Wait for curr to adapt
         # Set a voltage output just to be able to read current
-        while (abs(self.rpi.send_msg(READ, self.settings.command_lib['i_read']) - current*100) > CURR_OFF):
-            cunt = self.rpi.send_msg(READ, self.settings.command_lib['i_read'])/100
-            self.A_gauge.set_gauge(cunt)
+        while (abs(self.rpi.send_msg(READ, self.settings.command_lib['i_read']) != current*100):
+            # The current output current to be adjusted to the demanded value.
+            temp_curr = self.rpi.send_msg(READ, self.settings.command_lib['i_read'])/100
+            self.A_gauge.set_gauge(temp_curr)
             sleep(0.2)
             self.root.update()
         self.prompt.set_text("Ok, please wait!")
