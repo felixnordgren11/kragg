f0d9828a0e684e78bb676a0558c5a95e8dd8980c
miccls <martincsvardsjo@gmail.com>
2023-08-02 15:44:59 +0200

    fixed sensitivity adjustment

--- main.py
+++ main.py
@@ -232,8 +232,8 @@ class GUI:
         self.rpi.pin_a.when_pressed = self.rpi.pin_a_rising
         self.rpi.pin_b.when_pressed = self.rpi.pin_b_rising
         # Make it possible to change sensitivity
-        self.pin_d.when_pressed = lambda x: self.GUI.move_pointer('Left')
-        self.pin_e.when_pressed = lambda x: self.GUI.move_pointer('Right')
+        self.rpi.pin_d.when_pressed = lambda x: self.GUI.move_pointer('Left')
+        self.rpi.pin_e.when_pressed = lambda x: self.GUI.move_pointer('Right')
 
         self.gges['V_gauge'].set_active(Fact)
         # Increase sensitivity.
