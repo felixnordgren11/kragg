94c044ad95730cf29d42cbdd459b3b913f7a90f2
felixnordgren11 <felix.edu.nordgren@gmail.com>
2023-08-04 15:44:30 +0200

    ohje

--- main.py
+++ main.py
@@ -363,7 +363,7 @@ class GUI:
             self.root.after(self.settings.update_speed*2, self._update_value)
             return
         
-        if (not self.rpi.pin_v.is_pressed and not self.rpi.pin_i.is_pressed):
+        if ( self.rpi.pin_v.is_pressed and self.rpi.pin_i.is_pressed):
             self.calibration_procedure()
             
         # Send v_read a
